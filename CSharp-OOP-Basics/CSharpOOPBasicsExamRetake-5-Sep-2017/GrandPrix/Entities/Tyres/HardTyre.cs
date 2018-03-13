8ab2f19b8ae006191de90eef5eb0a1c1672e291c
RosenBobchev <rosenbobchev@abv.bg>
2018-03-13 20:05:26 +0200

    Working but not all functions

---
+++ CSharp-OOP-Basics/CSharpOOPBasicsExamRetake-5-Sep-2017/GrandPrix/Entities/Tyres/HardTyre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class HardTyre : Tyre
+{
+    private const string HARDTYRENAME = "Hard";
+
+    public HardTyre(double hardness)
+        : base(hardness)
+    {
+    }
+
+    public override string Name => HARDTYRENAME;
+}
+
