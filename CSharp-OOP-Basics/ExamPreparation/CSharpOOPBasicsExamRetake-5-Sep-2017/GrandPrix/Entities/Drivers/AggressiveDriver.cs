22c65cb70b5f9594d437ede6c8458a6b28df02ef
RosenBobchev <rosenbobchev@abv.bg>
2018-03-14 20:24:42 +0200

    update

--- CSharp-OOP-Basics/ExamPreparation/CSharpOOPBasicsExamRetake-5-Sep-2017/GrandPrix/Entities/Drivers/AggressiveDriver.cs
+++ CSharp-OOP-Basics/ExamPreparation/CSharpOOPBasicsExamRetake-5-Sep-2017/GrandPrix/Entities/Drivers/AggressiveDriver.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-public class AggressiveDriver : Driver
+﻿public class AggressiveDriver : Driver
 {
     private const double FuelConsumption = 2.7;
 
--- CSharp-OOP-Basics/ExamPreparation/CSharpOOPBasicsExamRetake-5-Sep-2017/GrandPrix/Entities/Drivers/Driver.cs
+++ CSharp-OOP-Basics/ExamPreparation/CSharpOOPBasicsExamRetake-5-Sep-2017/GrandPrix/Entities/Drivers/Driver.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-public abstract class Driver
+﻿public abstract class Driver
 {
     private string name;
     private double totalTime;
